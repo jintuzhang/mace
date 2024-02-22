26ba33c38246f3cbb0c7fd17046ae550372e3b54
jintuzhang <119382168+jintuzhang@users.noreply.github.com>
2024-02-22 06:05:06 +0100

    Finished charge model for LAMMPS (python part).

--- mace/cli/create_lammps_model.py
+++ mace/cli/create_lammps_model.py
@@ -1,20 +1,52 @@
+import os
 import sys
+import warnings
 
 import torch
 from e3nn.util import jit
 
-from mace.calculators import LAMMPS_MACE
+from mace.calculators import LAMMPS_MACE, LAMMPS_MACE_CHARGE
 
 
 def main():
-    assert len(sys.argv) == 2, f"Usage: {sys.argv[0]} model_path"
+    assert len(sys.argv) in [2, 3], f"Usage: {sys.argv[0]} model_path [charge_cv_expr]"
 
     model_path = sys.argv[1]  # takes model name as command-line input
     model = torch.load(model_path)
     model = model.double().to("cpu")
-    lammps_model = LAMMPS_MACE(model)
-    lammps_model_compiled = jit.compile(lammps_model)
-    lammps_model_compiled.save(model_path + "-lammps.pt")
+    model_name = model._get_name()
+    if (len(sys.argv) == 2):
+        if (model_name not in ['EnergyChargesMACE', 'AtomicsChargesMACE']):
+            lammps_model = LAMMPS_MACE(model)
+            lammps_model_compiled = jit.compile(lammps_model)
+            lammps_model_compiled.save(model_path + "-lammps.pt")
+        else:
+            message = 'Charge CV expression was not given! Will not ' + \
+                      'calculate charge CV with in the model!'
+            warnings.warn(message)
+            lammps_model = LAMMPS_MACE_CHARGE(model)
+            lammps_model_compiled = jit.compile(lammps_model)
+            lammps_model_compiled.save(model_path + "-lammps_charge.pt")
+    else:
+        if (model_name not in ['EnergyChargesMACE', 'AtomicsChargesMACE']):
+            message = '{:s} is not a charge model! Will ignore the given ' + \
+                      'charge CV expression!'
+            warnings.warn(message.format(sys.argv[1]))
+            lammps_model = LAMMPS_MACE(model)
+            lammps_model_compiled = jit.compile(lammps_model)
+            lammps_model_compiled.save(model_path + "-lammps.pt")
+        else:
+            # This is so crap, I am so retarded ...
+            with open('charge_cv_expr.py', 'w') as fp:
+                function = 'import torch\n@torch.jit.script\n'
+                function += 'def function(c: torch.Tensor) -> torch.Tensor:\n'
+                function += '    return {:s}\n'.format(sys.argv[2])
+                function += 'expr="{:s}"\n'.format(sys.argv[2])
+                print(function, file=fp)
+            lammps_model = LAMMPS_MACE_CHARGE(model)
+            lammps_model_compiled = jit.compile(lammps_model)
+            lammps_model_compiled.save(model_path + "-lammps_charge.pt")
+            os.remove('charge_cv_expr.py')
 
 
 if __name__ == "__main__":
